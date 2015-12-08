7e13e1099f507d607dc56eeaea77c93b7f66573a
franostanisicserato <frano.stanisic@serato.com>
2015-12-08 13:30:22 +1300

    Code cleanup: indenting, log messages and other cleanups from other pull requests.

--- src/main/java/org/jenkinsci/plugin/Diskcheck.java
+++ src/main/java/org/jenkinsci/plugin/Diskcheck.java
@@ -19,194 +19,172 @@ import java.io.PrintStream;
 import java.net.UnknownHostException;
 import java.util.List;
 
-
-
 /**
  * Class to allow any build step to be performed before the SCM checkout occurs.
- * 
- * @author Manoj Thakkar edited by Frano Stanisic
+ *
+ * @author Manoj Thakkar
+ * @author Frano Stanisic (8th Decemeber 2015, updated deletion to use Jenkins API/only delete files up to a threshhold)
  * 
  */
-
-
 public class Diskcheck extends BuildWrapper {
 
+    public final boolean failOnError;
 
-	public final boolean failOnError;
-        
-        public static FilePath lastFileModified(PrintStream log, FilePath dir) throws UnknownHostException, IOException, InterruptedException {
-            log.println("lastFileModified call made");
-            log.println("dir passed was " + dir);
-            List<FilePath> files = dir.listDirectories();
-            long lastMod = Long.MAX_VALUE;
-            FilePath choice = null;
-            for (FilePath file : files) {
-                if (file.lastModified() < lastMod) {
-                    choice = file;
-                    lastMod = file.lastModified();
+    public static FilePath lastFileModified(PrintStream log, FilePath dir) throws UnknownHostException, IOException, InterruptedException {
+        log.println("Determining file to delete ...");
+        List<FilePath> files = dir.listDirectories();
+        long lastMod = Long.MAX_VALUE;
+        FilePath choice = null;
+        for (FilePath file : files) {
+            if (file.lastModified() < lastMod) {
+                choice = file;
+                lastMod = file.lastModified();
+            }
+        }
+        return choice;
+    }
+
+    /**
+     * Constructor taking a list of buildsteps to use.
+     *
+     * @param buildstep list of build steps configured in the UI
+     */
+    @DataBoundConstructor
+    public Diskcheck(boolean failOnError) {
+        this.failOnError = failOnError;
+    }
+
+    /**
+     * Overridden setup returns a noop class as we don't want to add anything
+     * here.
+     *
+     * @param build
+     * @param launcher
+     * @param listener
+     * @return noop Environment class
+     */
+    @Override
+    public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
+        return new NoopEnv();
+    }
+
+    /**
+     * Overridden precheckout step, this is where we do all the work.
+     *
+     * Checks to make sure we have some buildsteps set, and then calls the
+     * prebuild and perform on all of them.
+     *
+     * @todo handle build steps failure in some sort of reasonable way
+     *
+     * @param build
+     * @param launcher
+     * @param listener
+     */
+    @Override
+    public Descriptor getDescriptor() {
+        return (Descriptor) super.getDescriptor();
+    }
+
+    @Override
+    public void preCheckout(AbstractBuild build, Launcher launcher,
+            BuildListener listener) throws IOException, InterruptedException {
+        PrintStream log = listener.getLogger();
+        // Default value of disk space check is 1GB		
+        int SpaceThreshold = PluginImpl.getInstance().getSpacecheck();
+
+        log.println("Disk space upper threshold is set to :" + SpaceThreshold + "GB");
+        log.println("Checking disk space now ...");
+
+        // Touch workspace so that it is created on first time.
+        if (!build.getWorkspace().exists()) {
+            build.getWorkspace().mkdirs();
+        }
+
+        Node node1 = build.getBuiltOn();
+        Computer Comp = node1.toComputer();
+        String NodeName = build.getBuiltOnStr();
+
+        // This new implementation doesn't even touch this, but given I
+        // am not 100% on what it does I have just left it here.
+        if (DiskSpaceMonitor.DESCRIPTOR.get(Comp) == null) {
+            log.println("No slave data available, waiting and retrying ...");
+            Thread.sleep(10000);
+            if (DiskSpaceMonitor.DESCRIPTOR.get(Comp) == null) {
+                log.println("Could not retrieve slave data, exiting disk check for this slave.");
+            }
+            System.exit(0);
+        }
+        // End unsure bit.
+
+        long size = build.getWorkspace().getParent().getUsableDiskSpace();
+        int roundedSize = (int) (size / (1024 * 1024 * 1024));
+        log.println("Total disk space available is: " + roundedSize + "GB");
+
+        if ("".equals(NodeName)) {
+            NodeName = "master";
+        }
+
+        log.println("Node Name: " + NodeName);
+        boolean diskrecyclerenabled = PluginImpl.getInstance().isDiskrecyclerenabled();
+        if (diskrecyclerenabled) {
+            if (roundedSize < SpaceThreshold) {
+                int clearUntil = PluginImpl.getInstance().getClearuntil();
+                if (clearUntil == 0) {
+                    log.println("clearUntil set to 0, clearing everything ...");
+                    // Jenkins api recursive delete, errors will cause a build failure.
+                    build.getWorkspace().getParent().deleteRecursive();
+                    log.println("All workspaces deleted successfully.");
+                } else {
+                    if (clearUntil < SpaceThreshold) {
+                        throw new AbortException(
+                                "Your space clearing thresholds are the wrong way around."
+                                + " Edit them and run this job again.");
+                    }
+                    // Not enough room on slave, start clearing until threshhold met.
+                    while (roundedSize < clearUntil) {
+                        FilePath lastUsedDir = lastFileModified(log, build.getWorkspace().getParent());
+                        if (lastUsedDir == null) {
+                            throw new AbortException(
+                                    "There is nothing left in the workspace, but room still needs to be made."
+                                    + " Either the thresholds are wrong, or there is an issue on the slave.");
+                        }
+                        log.println("Deleting workspace " + lastUsedDir.getRemote() + ".");
+                        // Jenkins api recursive delete, errors will cause a build failure.
+                        lastUsedDir.deleteRecursive();
+                        log.println("Workspace deleted.");
+                        size = build.getWorkspace().getParent().getUsableDiskSpace();
+                        roundedSize = (int) (size / (1024 * 1024 * 1024));
+                        log.println("New disk space available is: " + roundedSize + "GB");
+                    }
                 }
             }
-            return choice;
-        }        
-	/**
-	 * Constructor taking a list of buildsteps to use.
-	 * 
-	 * @param buildstep
-	 *            list of build steps configured in the UI
-	 */
-	@DataBoundConstructor
-	public Diskcheck(boolean failOnError) {
-		this.failOnError = failOnError;
-	}
-
-	/**
-	 * Overridden setup returns a noop class as we don't want to add annything
-	 * here.
-	 * 
-	 * @param build
-	 * @param launcher
-	 * @param listener
-	 * @return noop Environment class
-	 */
-	@Override
-	public Environment setUp(AbstractBuild build, Launcher launcher,
-			BuildListener listener) throws IOException, InterruptedException {
-		return new NoopEnv();
-	}
-
-	/**
-	 * Overridden precheckout step, this is where we do all the work.
-	 * 
-	 * Checks to make sure we have some buildsteps set, and then calls the
-	 * prebuild and perform on all of them.
-	 * 
-	 * @todo handle build steps failure in some sort of reasonable way
-	 * 
-	 * @param build
-	 * @param launcher
-	 * @param listener
-	 */
-	
-	 @Override
-		public Descriptor getDescriptor() {
-	        return (Descriptor) super.getDescriptor();
-	    }
-                
-	@Override
-	public void preCheckout(AbstractBuild build, Launcher launcher,
-			BuildListener listener) throws IOException, InterruptedException {
-		PrintStream log = listener.getLogger();
-// Default value of disk space check is 1Gb		
-		int SpaceThreshold = PluginImpl.getInstance().getSpacecheck();
-		
-
-		log.println("Disk space threshold is set to :" + SpaceThreshold + "Gb");
-		log.println("Checking disk space Now ");
-
-		/* touch workspace so that it is created on first time */
-		if (!build.getWorkspace().exists()) {
-			build.getWorkspace().mkdirs();
-		}
-
-		Node node1 = build.getBuiltOn();
-		Computer Comp = node1.toComputer();
-		String NodeName = build.getBuiltOnStr();
-    /*    if  (Comp.getChannel()==null)
-        {
-        	log.println("Can not get slave infomration wait for 10 sec \n");
-        	Thread.sleep(10000);
-        	 if (Comp.getChannel()==null)
-        	 {
-        		 log.println("Waited long enough to get slave information exiting discheck for now \n");
-        	 System.exit(0);
-        	 }
-        	 
         }
-        */
-		
-        if ( DiskSpaceMonitor.DESCRIPTOR.get(Comp)== null )
-        {   log.println("No Slave Data available trying to get data from slave");
-            Thread.sleep(10000);
-            if ( DiskSpaceMonitor.DESCRIPTOR.get(Comp)== null )
-            	
-        	log.println(" Could not get Slave Information , Exiting Disk check for this slave");
-        	System.exit(0);
+
+        log.println("Running prebuild steps ...");
+        if (roundedSize < SpaceThreshold
+                && !diskrecyclerenabled) {
+            throw new AbortException(
+                    "Disk space is too low, investigate prior to starting another build.");
+
         }
-        
-		long size = build.getWorkspace().getParent().getUsableDiskSpace();
-		int roundedSize = (int) (size / (1024 * 1024 * 1024));
-		log.println("Total Disk Space Available is: " + roundedSize + "Gb");
+    }
 
-		if (build.getBuiltOnStr() == "") {
-			NodeName = "master";
-		}
+    @Extension
+    public static final class DescriptorImpl extends Descriptor<BuildWrapper> {
 
-		log.println(" Node Name: " + NodeName);
-	
-		if (PluginImpl.getInstance().isDiskrecyclerenabled()) {
-			if (roundedSize < SpaceThreshold) {
-                                int clearUntil = PluginImpl.getInstance().getClearuntil();
-                                if (clearUntil == 0){
-                                    log.println("clearUntil set to 0, clearing everything...");
-                                    build.getWorkspace().getParent().deleteRecursive();
-                                    log.println("aaaaaaaaand it's gone.");
-                                }else{
-                                if (clearUntil < SpaceThreshold){
-                                    throw new AbortException(
-					"Your space clearing thresholds are the wrong way around."
-                                                + " Edit them and run this job again.");
-                                }
-                                // Not enough room on slave, start clearing until threshhold met.
-                                while(roundedSize < clearUntil){
-                                FilePath lastUsedDir = lastFileModified(log, build.getWorkspace().getParent());
-                                if (lastUsedDir == null){
-                                    throw new AbortException(
-					"There is nothing left in the workspace, but room still needs to be made."
-                                                + " Either the thresholds are wrong, or there is a major issue on the slave.");
-                                }
-                                log.println("Workspace selected was " + lastUsedDir.getRemote() + ". Goodbye!");
-				// Use Jenkins internal api to delete files recursively.
-				// On any errors the exception will bubble up causing the
-				// build to fail.
-				lastUsedDir.deleteRecursive();    
-                                size = build.getWorkspace().getParent().getUsableDiskSpace();
-                                roundedSize = (int) (size / (1024 * 1024 * 1024));
-                                log.println("New disk space available is: " + roundedSize);
-                        }
-			}
-                        }
-		}
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Check Disk Space";
+        }
 
-		log.println("Running Prebuild steps");
-		if (roundedSize < SpaceThreshold
-				&& !(PluginImpl.getInstance().isDiskrecyclerenabled())) {
-			throw new AbortException(
-					"Disk Space is too low please look into it before starting a build");
-
-		}
-	}
-
-	
-	@Extension
-	public static final class DescriptorImpl extends Descriptor<BuildWrapper> {
-	
-		/**
-		 * This human readable name is used in the configuration screen.
-		 */
-		public String getDisplayName() {
-				return "Check Disk Space";
-		}
-		
-		
-		
-		public DescriptorImpl() {
+        public DescriptorImpl() {
             load();
         }
-		
 
-	}
-	
-	
-	class NoopEnv extends Environment {
-	}
+    }
+
+    class NoopEnv extends Environment {
+    }
 }
--- src/main/java/org/jenkinsci/plugin/PluginImpl.java
+++ src/main/java/org/jenkinsci/plugin/PluginImpl.java
@@ -1,9 +1,6 @@
 package org.jenkinsci.plugin;
 
 import hudson.Plugin;
-import hudson.model.*;
-import hudson.model.AbstractBuild;
-import hudson.model.AbstractProject;
 import hudson.model.Descriptor.FormException;
 import hudson.model.Hudson;
 
@@ -15,14 +12,13 @@ import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.StaplerRequest;
 
-//import com.innorium.crystalline.plugins.model.ScannerJobProperty;
+public class PluginImpl extends Plugin {
 
-public class PluginImpl extends Plugin{
-	private boolean diskrecyclerenabled;
-	private int spacecheck;
-        private int clearuntil;
-	
-	 /**
+    private boolean diskrecyclerenabled;
+    private int spacecheck;
+    private int clearuntil;
+
+    /**
      * Returns the singleton instance.
      *
      * @return the one.
@@ -30,57 +26,52 @@ public class PluginImpl extends Plugin{
     public static PluginImpl getInstance() {
         return Hudson.getInstance().getPlugin(PluginImpl.class);
     }
-    
-	@Override
-	public void start() throws Exception {
-		super.start();
-		load();
-	}
-	
-	@Override
-	public void stop() throws Exception {
-		super.stop();
-	}
-	
-	@Override
-	public void configure(StaplerRequest req, JSONObject formData)
-			throws IOException, ServletException, FormException {
-		 formData=formData.getJSONObject("disk-check");
-		 spacecheck=formData.getInt("spacecheck");
-                 clearuntil=formData.getInt("clearuntil");
-		 diskrecyclerenabled=formData.getBoolean("diskrecyclerenabled");
-		
-		save();
-		super.configure(req, formData);
-	    
-	}
-	
-	
 
-	public void setDiskrecyclerenabled(boolean diskrecyclerenabled) {
-	this.diskrecyclerenabled = diskrecyclerenabled;
-}
-	
-	public int getSpacecheck()
-	{
-		return spacecheck;
-	}
-	
-	public void setSpacecheck(int spaceCheck) 
-	{
-	this.spacecheck = spaceCheck;
-}
-        public int getClearuntil()
-	{
-		return clearuntil;
-	}
-	
-	public void setClearuntil(int clearUntil) 
-	{
-	this.clearuntil = clearUntil;
-}
+    @Override
+    public void start() throws Exception {
+        super.start();
+        load();
+    }
 
-	public boolean isDiskrecyclerenabled() {
-		return diskrecyclerenabled;
-	}
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    @Override
+    public void configure(StaplerRequest req, JSONObject formData)
+            throws IOException, ServletException, FormException {
+        formData = formData.getJSONObject("disk-check");
+        spacecheck = formData.getInt("spacecheck");
+        clearuntil = formData.getInt("clearuntil");
+        diskrecyclerenabled = formData.getBoolean("diskrecyclerenabled");
+
+        save();
+        super.configure(req, formData);
+
+    }
+
+    public void setDiskrecyclerenabled(boolean diskrecyclerenabled) {
+        this.diskrecyclerenabled = diskrecyclerenabled;
+    }
+
+    public int getSpacecheck() {
+        return spacecheck;
+    }
+
+    public void setSpacecheck(int spaceCheck) {
+        this.spacecheck = spaceCheck;
+    }
+
+    public int getClearuntil() {
+        return clearuntil;
+    }
+
+    public void setClearuntil(int clearUntil) {
+        this.clearuntil = clearUntil;
+    }
+
+    public boolean isDiskrecyclerenabled() {
+        return diskrecyclerenabled;
+    }
 }
